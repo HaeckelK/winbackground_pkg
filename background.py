1e622c99721b365192681e04feae9ec90e925cf7
HaeckelK <noneofyour@biz.com>
2020-01-18 18:52:53 +0000

    change functionality and basic testing

--- background.py
+++ background.py
@@ -2,17 +2,82 @@ import ctypes
 import os
 from random import randrange
 
-import config
 
-IMAGE_FOLDER = config.OUTPUT_FOLDER
+# IMAGE_FOLDER = config.OUTPUT_FOLDER
+
+SPI_SETDESKWALLPAPER = 20
+
+class Background():
+    def __init__(self):
+        self.filetypes = ['jpg']
+        return
+
+    def change(self, file):
+        """
+        Change desktop wallpaper to image located at file.
+        
+        Parameters
+        ----------
+        file : str
+            Full file path (not relative) to image file.
+        """
+
+        # TODO : insist on type of image
+        
+        # Check that file exists and is absolute path
+        check_passed = self._check_file(file)
+        if check_passed == False:
+            return False
+
+        self._change_background(file)
+        return True
+
+    def _check_file(self, file):
+        if not isinstance(file, str):
+            print('file must be a string')
+            return False
+
+        if os.path.isfile(file) is False:
+            print('file does not exist', file)
+            return False
+
+        if os.path.isabs(file) is False:
+            print('file path must be absolute not relative', file)
+            return False
+
+        return True
+
+    def _change_background(self, file):
+        """
+        Parameters
+        ----------
+        file : str
+            Full file path (not relative) to image file.
+        """
+        """
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0,
+                                                   file, 0)
+        """
+        print('original called')
+        return
+
+def change_function(file):
+    """
+    Parameters
+    ----------
+    file : str
+        Full file path (not relative) to image file.
+    """
+    # TODO File must be full file path?
+    result = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER,
+                                                        0, file, 0)
+    if not result:
+        print('File doesnt exist')
+    return result
 
 def main():
     photos = os.listdir(IMAGE_FOLDER)
     photos = [os.path.join(IMAGE_FOLDER, photo) for photo in photos]
     file = photos[randrange(len(photos))]
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, file, 3)
+    
     return
-
-if __name__ == '__main__':
-    # main()
-    pass
--- test/test_background.py
+++ test/test_background.py
@@ -1,4 +1,44 @@
 import pytest
+import os
 
 from context import background
-from police_stop_and_search_api.police_api import PoliceAPI
+from background import Background
+
+def mock_change_background(self, file):
+    print('mock_change_background')
+    return
+
+#monkeypatch.setattr(PoliceAPI, "_get_response", mock_response_404)
+
+@pytest.fixture
+def bg(monkeypatch):
+    monkeypatch.setattr(Background, '_change_background',
+                        mock_change_background)
+    bk = Background()
+    return bk
+
+@pytest.mark.parametrize('file, expected', [
+    ('', False),
+    ('x', False),
+    (int, False),
+    (float, False),
+    ('test\\a.jpg', False),
+    (os.path.abspath('fixtures\\image.jpg'), True),
+    ('fixtures\\image.jpg', False)
+])
+def test_check_file_str_path(file, expected):
+    """
+    Test is string, absolute path, exists.
+    """
+    bk = Background()
+    result = bk._check_file(file)
+    assert result == expected
+
+
+def test_temp(bg):
+    """
+    """
+    bk = bg
+    file = os.path.abspath('fixtures\\image.jpg')
+    result = bk.change(file)
+    assert result == True
